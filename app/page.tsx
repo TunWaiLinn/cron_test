a8df5ade7f0552887c3b1d5a4988e70089c89ef9
TunWaiLinn <tunwailinn007@gmail.com>
2024-05-31 12:33:12 +0630

    3rd commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 // app/page.js
 import React from "react";
 
-async function getWordOfTheDay() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/getWordOfTheDay`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch the word of the day");
-  }
-  const data = await res.json();
-  return data.word;
-}
+// async function getWordOfTheDay() {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/getWordOfTheDay`);
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch the word of the day");
+//   }
+//   const data = await res.json();
+//   return data.word;
+// }
 
 export default async function Home() {
-  const wordOfTheDay = await getWordOfTheDay();
+  // const wordOfTheDay = await getWordOfTheDay();
+  const wordOfTheDay = "Hello";
 
   return (
     <div>
